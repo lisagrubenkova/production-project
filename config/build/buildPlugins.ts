e10e47c630336a170689948c9f9d9d90bba5536a
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-11-08 18:41:24 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,12 +2,13 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -19,6 +20,14 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
   ]
+  
+  if (isDev) {
+  plugins.push(new webpack.HotModuleReplacementPlugin())
+  plugins.push(new BundleAnalyzerPlugin({
+    openAnalyzer: false
+  }))
+  }
+
+  return plugins
 }
