c0d749ce43511b248fcbb6777e9920795d19c997
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-11-10 14:32:20 +0300

    add visual tool, fix screens

---
+++ scripts/generate-visual-json-report.js
@@ -0,0 +1,31 @@
+import { promisify } from 'util';
+import { readdir, writeFile } from 'fs';
+import { join as joinPath, relative } from 'path';
+
+const asyncReaddir = promisify(readdir);
+const writeFileAsync = promisify(writeFile);
+
+const __dirname = import.meta.dirname;
+
+// eslint-disable-next-line no-undef
+const lokiDir = joinPath(__dirname, '..', '.loki');
+const actualDir = joinPath(lokiDir, 'current');
+const expectedDir = joinPath(lokiDir, 'reference');
+const diffDir = joinPath(lokiDir, 'difference');
+
+(async function main() {
+    const diffs = await asyncReaddir(diffDir);
+
+    await writeFileAsync(joinPath(lokiDir, 'report.json'), JSON.stringify({
+        newItems: [],
+        deletedItems: [],
+        passedItems: [],
+        failedItems: diffs,
+        expectedItems: diffs,
+        actualItems: diffs,
+        diffItems: diffs,
+        actualDir: relative(lokiDir, actualDir),
+        expectedDir: relative(lokiDir, expectedDir),
+        diffDir: relative(lokiDir, diffDir),
+    }));
+}());
